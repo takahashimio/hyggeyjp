bf331abd5a2403672c50ddced386186df03e842d
HYGGEY <takahashimio.work@gmail.com>
2021-07-24 14:22:54 +0900

    seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `HYGGEY`,
+    description: `心と暮らすシンプルライフ`,
+    lang: `ja`,
+    siteUrl: `https://hyggey.jp`,
+    locale: `ja_JP`
+  },
+
+  /* Your site config here */
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -16,5 +25,22 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+
+    `gatsby-plugin-react-helmet`,
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `HYGGEY ヒュギー`,
+        short_name: `HYGGEY`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#477294`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+
+    `gatsby-plugin-offline`,
   ],
 }
